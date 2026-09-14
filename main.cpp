6c9336f00c96621222f6088df6573625d9af4d75
raul <00025226@uca.edu.sv>
2026-09-14 12:09:20 -0600

    Estado determinado

--- main.cpp
+++ main.cpp
@@ -8,24 +8,47 @@ struct Elemento
     int Longitud;
     float cargas[3];
     int Factor, capacidadMaxima;
-    bool Estado;
+    string Estado;
 };
 
-float calcularFactor(Elemento *elemento){
+float calcularFactor(Elemento *elemento)
+{
     float promedio;
-    
-    promedio = (elemento->cargas[0]+elemento->cargas[1]+elemento->cargas[2]) / 3;
 
-    float Factor = promedio/elemento->capacidadMaxima;
+    promedio = (elemento->cargas[0] + elemento->cargas[1] + elemento->cargas[2]) / 3;
+
+    float Factor = promedio / elemento->capacidadMaxima;
 
     return Factor;
+}
+
+void determinarSeguridad(Elemento *elemento)
+{
+    float factor = elemento->Factor;
+
+    if (factor >= 0.00 && factor <= 0.50)
+    {
+        elemento->Estado = "Seguro";
+    }
+    else if (factor > 0.50 && factor <= 0.80)
+    {
+        elemento->Estado = "Precaucion";
+    }
+    else if (factor > 0.80 && factor <= 1.0)
+    {
+        elemento->Estado = "Riesgo";
+    }
+    else if (factor > 1.00)
+    {
+        elemento->Estado = "SOBRECARGA";
+    }
 }
 
 void registrarElemento(Elemento &elemento)
 {
     cout << "Cual es el codigo de su Elemento?" << endl;
     cin >> elemento.codigoElemento;
-    
+
     cout << "Cual es el nombre de su Elemento?" << endl;
     cin >> elemento.nombreElemento;
 
@@ -47,9 +70,13 @@ void registrarElemento(Elemento &elemento)
     cout << endl;
     cout << "Factor de utilizacion: " << calcularFactor(&elemento) << endl;
     cout << endl;
+
+    determinarSeguridad(&elemento);
+
+    cout << endl;
+    cout << "Estado de seguridad: " << elemento.Estado << endl;
+    cout << endl;
 }
-
-
 
 int main()
 {
@@ -68,9 +95,10 @@ int main()
         }
     } while (cantidad < 1 || cantidad > 10);
 
-    for(int i = 0; i < cantidad; i++){
+    for (int i = 0; i < cantidad; i++)
+    {
         cout << endl;
-        cout << i+1 << ". Elemento: " << endl;
+        cout << i + 1 << ". Elemento: " << endl;
         cout << endl;
         registrarElemento(elementos[i]);
     }
